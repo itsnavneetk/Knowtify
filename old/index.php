d844d98d217e5c515216e5a02c18807b107a15c5
Navneet Kishan <itsnavneetk@gmail.com>
2017-10-27 17:14:21 +0530

    fixed fetching locations from DB

--- old/index.php
+++ old/index.php
@@ -9,7 +9,7 @@ session_start();
 -->
 <html>
 <head>
-  <title>Identity by HTML5 UP</title>
+  <title>Knowtify</title>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <!--[if lte IE 8]><script src="assets/js/html5shiv.js"></script><![endif]-->
@@ -27,18 +27,70 @@ session_start();
     <section id="main" style="min-width: 47em">
 
       <header>
-         <span class="avatar"><img src="https://tracker.moodle.org/secure/useravatar?size=small&avatarId=17388" width="125px" height="125px" />
-            <!--https://jira.hyperledger.org/secure/useravatar?size=xsmall&avatarId=10341-->
-            <h3 style="font-weight: bold;margin-top: 10px;"><?php echo $_SESSION["username"]; ?></h3>
-        </span>
-    </header>
+       <span class="avatar"><img src="https://tracker.moodle.org/secure/useravatar?size=small&avatarId=17388" width="125px" height="125px" />
+        <!--https://jira.hyperledger.org/secure/useravatar?size=xsmall&avatarId=10341-->
+        <h3 style="font-weight: bold;margin-top: 10px;"><?php echo $_SESSION["username"]; ?></h3>
+    </span>
+</header>
 
-    <div id="mapCanvas" style="width:100%;height:500px"></div>
-    <footer>
-     <ul class="icons">
+<div id="mapCanvas" style="width:100%;height:500px"></div>
 
-     </ul>
- </footer>
+<?php
+        $dbhost="localhost";
+        $dbname="knowtify";
+        $dbuser="root";
+        $dbpass="";
+        $conn=mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+        if(mysqli_connect_errno()){
+            die("database connection failed:".mysqli_connect_error()."(".mysqli_connect_errno().")");
+        }
+        $query="SELECT * FROM location";
+        $result=mysqli_query($conn,$query);
+
+        $coordsPHP = array();
+        $lat = array();
+        $long = array();
+        $place = array();
+        $k = 0;
+if (mysqli_num_rows($result) == 0) {
+    echo "sorry";}
+    else{
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "lat : ".$row["lat"]."</br>"."long : ".$row["longi"]."</br>"."Place : ".$row["place"]."</br>"."</br>";
+        $lat[$k] = $row["lat"];
+        $long[$k] = $row["longi"];
+        $place[$k] = $row["place"];
+        
+        $coordsPHP[$k][0] = $place[$k];
+        $coordsPHP[$k][1] =  $lat[$k];
+        $coordsPHP[$k][2] =  $long[$k];
+        $k = $k +1;
+
+    }
+    
+}
+
+
+echo count($lat);
+echo count($long);
+echo count($place);
+echo "***entries fetched by php***";
+for ($row = 0; $row < 3; $row++) {
+  echo "<p><b>Row number $row</b></p>";
+  echo "<ul>";
+  for ($col = 0; $col < 3; $col++) {
+    echo "<li>".$coordsPHP[$row][$col]."</li>";
+  }
+  echo "</ul>";
+}
+?>
+
+
+<footer>
+   <ul class="icons">
+
+   </ul>
+</footer>
 </section>
 <div>
     <button><a href="./index.php">Refersh!</a></button>
@@ -59,22 +111,6 @@ session_start();
 
 </div>
 
-<!-- Scripts -->
-<!--[if lte IE 8]><script src="assets/js/respond.min.js"></script><![endif]-->
-	<!--		
-<script>
-function myMap() {
-  var mapCanvas = document.getElementById("map");
-  var mapOptions = {
-    center: new google.maps.LatLng(45.434046,12.340284),
-    zoom:18,
-    
-  };
-  var map = new google.maps.Map(mapCanvas,mapOptions);
-  map.setTilt(100);
-}
-</script>
--->
 
 <script>
     function initMap() {
@@ -89,7 +125,6 @@ function myMap() {
     map.setTilt(50);
 
 
-
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
@@ -100,20 +135,27 @@ function error(msg) {
     alert('error in geolocation');
 }
 function showPosition(position) {
-   lats = position.coords.latitude;
-   lngs = position.coords.longitude; 
+ lats = position.coords.latitude;
+ lngs = position.coords.longitude; 
 
-   function success(position) {
-       lats = position.coords.latitude;
-       lngs = position.coords.longitude;
+ function success(position) {
+     lats = position.coords.latitude;
+     lngs = position.coords.longitude;
   //alert(lats);
   //alert(lngs);
   var tempcord = [lats, lngs];
   return tempcord;
 };
+//fetching coords from the db
+    
+
+
+
+var markers = <?php echo json_encode( $coordsPHP ) ?>;
+var mymarker = ['My location', lats, lngs];
 
     // Multiple markers location, latitude, and longitude
-    var markers = [
+/*    var markers = [
     ['My location', lats, lngs],
     ['Food Court', 13.346335, 74.794151],
     ['Caffeteria', 13.352028, 74.793178],
@@ -123,84 +165,85 @@ function showPosition(position) {
     ['MIT Canteen', 13.351227, 74.794204]
 
     ];
-
+*/
     // Info window content
+    var myinfoWindowContent =['<div class="info_content">' +
+    '<h3>ME</h3>' +
+    '<p></p>' + '</div>']; 
+
     var infoWindowContent = [
     ['<div class="info_content">' +
-    '<h3>ME</h3>' +
-    '<p></p>' + '</div>'],
-    ['<div class="info_content">' +
-    '<h3>Food Court</h3>' +
+    '<h3>'+markers[0][0]+'</h3>' +
     '<h5>Restaurant</h5>'+
     '<p>The ambience of this place is just amazing coupled with the quality food.</p>'+'<p><h3>HOT OFFERS</h3><h3>Happy Hours</h3></p>'+'<button onclick="myFunction(1)" >NOTIFY!</button>'+
     '</div>'],
     ['<div class="info_content">' +
-    '<h3>Caffeteria</h3>' +
+    '<h3>'+markers[1][0]+'</h3>' +
     '<h5>Restaurant</h5>'+
     '<p>Decent food at affordable rates and the staff are friendly</p>'+'<p><h3>HOT OFFERS</h3><h3>Happy Hours</h3></p>'+'<button onclick="myFunction(2)">NOTIFY!</button>'+
     '</div>'],
     ['<div class="info_content">' +
-    '<h3>Aditya Mess</h3>' +
+    '<h3>'+markers[2][0]+'</h3>' +
     '<h5>Restaurant</h5>'+
     '<p>Varied options and low prices plus food quality is amazing</p>'+'<p><h3>HOT OFFERS</h3><h3>Happy Hours</h3></p>'+'<button onclick="myFunction(3)">NOTIFY!</button>'+
     '</div>'],
 
     ['<div class="info_content">' +
-    '<h3>Unexplored location</h3>' +
+    '<h3>'+markers[3][0]+'</h3>' +
     '<h5>Retail shop</h5>'+
     '<p>----</p>' +
     '</div>'],
 
     ['<div class="info_content">' +
-    '<h3>Out of bounds</h3>' +
+    '<h3>'+markers[4][0]+'</h3>' +
     '<h5>General Shop</h5>'+
     '<p>----</p>' +
     '</div>'],
 
     ['<div class="info_content">' +
-    '<h3>MIT Canteen</h3>' +
+    '<h3>'+markers[5][0]+'</h3>' +
     '<h5>Restaurant</h5>'+
     '<p>They offer rolls, baked cakes, samosas, puffs and coffee/tea </p>'+'<p><h3>HOT OFFERS</h3><h3>Happy Hours</h3></p>'+'<button onclick="myFunction(4)">NOTIFY!</button>'+
     '</div>']
     ];
 
-     var image = {
-          url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+    var image = {
+      url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
           // This marker is 20 pixels wide by 32 pixels high.
           size: new google.maps.Size(20, 32),
           // The origin for this image is (0, 0).
           origin: new google.maps.Point(0, 0),
           // The anchor for this image is the base of the flagpole at (0, 32).
           anchor: new google.maps.Point(0, 32)
-        };
+      };
     // Add multiple markers to map
     var infoWindow = new google.maps.InfoWindow(), marker, i;
-    
+//http://map-icons.com/ get icons from here**    
     //user location marker
-    var position = new google.maps.LatLng(markers[0][1], markers[0][2]);
+    var position = new google.maps.LatLng(mymarker[1], mymarker[2]);
     bounds.extend(position);
-    marker = new google.maps.Marker({
+    mymarker = new google.maps.Marker({
         position: position,
         map: map,
-          icon: {
+        icon: {
             path: google.maps.SymbolPath.CIRCLE,
             scale: 10
-          },
-        center: new google.maps.LatLng(lats, lngs),
+        },
+        center: new google.maps.LatLng(mymarker[1], mymarker[2]),
         center: {lat: lats, lng: lngs},
-        title: markers[0][0]
+        title: mymarker[0]
     });
          // Add info window to marker    
-         google.maps.event.addListener(marker, 'click', (function(marker, i) {
+         google.maps.event.addListener(mymarker, 'click', (function(mymarker, i) {
             return function() {
-                infoWindow.setContent(infoWindowContent[i][0]);
-                infoWindow.open(map, marker);
+                infoWindow.setContent(myinfoWindowContent[0]);
+                infoWindow.open(map, mymarker);
             }
-        })(marker, 0));
+        })(mymarker, 0));
 
 
     // Place each marker on the map  
-    for( i = 1; i < markers.length; i++ ) {
+    for( i = 0; i < markers.length; i++ ) {
         var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
         bounds.extend(position);
         marker = new google.maps.Marker({
@@ -208,7 +251,7 @@ function showPosition(position) {
             map: map,
             draggable: true,
             animation: google.maps.Animation.DROP,
-            center: new google.maps.LatLng(lats, lngs),
+            center: new google.maps.LatLng(mymarker[1], mymarker[2]),
             center: {lat: lats, lng: lngs},
             title: markers[i][0]
         });
@@ -222,10 +265,10 @@ function showPosition(position) {
         })(marker, i));
 
         function toggleBounce() {
-         function drop() {
-        for (var i = 1; i < markers.length; i++) {
-          addMarkerWithTimeout(markers[i], i * 200);
-        }
+           function drop() {
+            for (var i = 1; i < markers.length; i++) {
+              addMarkerWithTimeout(markers[i], i * 200);
+          }
       }
 
       function addMarkerWithTimeout(position, timeout) {
@@ -234,10 +277,10 @@ function showPosition(position) {
             position: position,
             map: map,
             animation: google.maps.Animation.DROP
-          }));
-        }, timeout);
-      }
+        }));
+      }, timeout);
     }
+}
         // Center the map to fit all markers on the screen
         map.fitBounds(bounds);
     }
@@ -264,9 +307,9 @@ function showPosition(position) {
 
 <script>
     if ('addEventListener' in window) {
-       window.addEventListener('load', function() { document.body.className = document.body.className.replace(/\bis-loading\b/, ''); });
-       document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
-   }
+     window.addEventListener('load', function() { document.body.className = document.body.className.replace(/\bis-loading\b/, ''); });
+     document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
+ }
 </script>
 
 </body>
